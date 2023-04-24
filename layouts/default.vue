b4039deffe677c6e5d976217ac09cfe483780d1f
Yan <yan.doshchinsky@gmail.com>
2023-04-24 18:24:19 +0600

    feat(layouts): default layout created

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="default-layout">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+};
+</script>
+
+<style lang="scss" scoped>
+.default-layout {
+
+}
+</style>
