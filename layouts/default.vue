8812e994e329c120f0961f9d9bb3ada60406f319
Yan <yan.doshchinsky@gmail.com>
2023-04-25 00:22:40 +0600

    feat(layout): layout template and styles updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="default-layout">
-    <nuxt />
+    <main class="default-layout__content">
+      <nuxt />
+    </main>
   </div>
 </template>
 
@@ -15,6 +17,12 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .default-layout {
-
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  &__content {
+    @include container;
+  }
 }
 </style>
