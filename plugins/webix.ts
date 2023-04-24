4e7b8614c030b290fa1ac4d29f436cc9a1c6a13a
Yan <yan.doshchinsky@gmail.com>
2023-04-24 23:10:53 +0600

    feat(webix): webix lib added, webix global injected

---
+++ plugins/webix.ts
@@ -0,0 +1,6 @@
+import * as webix from '~/libs/webix/webix';
+import type { Context, Inject } from '@nuxt/types/app';
+
+export default (_: Context, inject: Inject): void => {
+  inject('webix', webix);
+};
