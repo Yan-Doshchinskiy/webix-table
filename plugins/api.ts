5950abb1d72430f3de6ded0e37c12eb76f25d6b8
Yan <yan.doshchinsky@gmail.com>
2023-04-28 02:16:18 +0600

    fix(api): api module main url changed

--- plugins/api.ts
+++ plugins/api.ts
@@ -4,8 +4,8 @@ import MainModule from '~/core/api/modules/main.module';
 import type { IMainModuleResponse } from '~/core/api/types';
 
 export default (_: Context, inject: Inject): void => {
-  // const apiModule = new MainModule('https://http-test-two.vercel.app/api/v1', {});
-  const apiModule = new MainModule('http://localhost:5000/api/v1', {});
+  const apiModule = new MainModule('https://http-test-two.vercel.app/api/v1', {});
+  // const apiModule = new MainModule('http://localhost:5000/api/v1', {});
   apiModule.addResponseInterceptor<IMainModuleResponse>((response) => response.data.result);
   inject('api', apiModule.controllers);
 };
